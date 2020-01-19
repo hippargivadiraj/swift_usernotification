8ceac208df77914293916dbad42afb192ba8e2ef
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-01-19 09:39:49 -0800

    user notification

--- userNotificationExample/ContentView.swift
+++ userNotificationExample/ContentView.swift
@@ -7,10 +7,36 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Button("Request Permssion"){
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success ,  error ) in
+                    if success {
+                        print("allSet")
+                    }else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+                
+            }
+            
+          Button("Send Notification"){
+            
+            let content = UNMutableNotificationContent()
+            content.title = " Feed The Cat"
+            content.subtitle = "Cat is Hungry"
+            content.sound  = UNNotificationSound.default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request)
+                       
+                   }
+            
+        }
     }
 }
 
